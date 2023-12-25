cb57d421da25eaabe70f5bb806e7bc42898d1e7c
Денис Стычинский <stychynskidenis@gmail.com>
2023-12-25 14:05:58 +0300

    HM

---
+++ Program/Exceptions/Exc.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class Exc extends RuntimeException{
+    
+    public Exc (String msg) {
+        super(msg);
+    }
+}
---
+++ Program/Model/PetCr.java
@@ -0,0 +1,18 @@
+package Model;
+
+public class PetCr extends Creator {
+
+    @Override
+    protected Pet createNewPet (PetType type) {
+
+        switch (type) {
+            case Cat:
+                return new Cat();
+            case Dog:
+                return new Dog();
+            case Hamster:
+                return new Hamster();
+        }
+        return null;
+    }
+}
